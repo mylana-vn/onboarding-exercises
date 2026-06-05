892ea1e386be7299552f706099d38f02f3ec23ba
Mylana Van Name <mvanname@geir.astro.utoronto.ca>
2026-06-05 15:08:14 -0400

    added ratio plots, added LMC orbit path to slice plots

--- onboarding-exercises.py
+++ onboarding-exercises.py
@@ -579,74 +579,6 @@ def integrate_full(ro,vo,hq_df,mw_interp,lmc_moving,nif,N):
     stars_full.integrate(ts_fwd, [mw_interp,lmc_moving,nif], method="dop853_c")
     return stars_full
 
-def plot_radial_density_comparison(r_mw_final,r_full_final,N,filename="radial_density_comparsion_function.png"):
-
-    bins = np.geomspace(0.1,1000,50)
-    r_centres = 0.5 * (bins[:-1] + bins[1:])
-    volumes = (4.0/3.0) * np.pi * (bins[1:]**3 - bins[:-1]**3)
-
-    counts_mw, edges_mw = np.histogram(r_mw_final,bins=bins) 
-    counts_full, edges_full = np.histogram(r_full_final,bins=bins) 
-
-    n_mw = counts_mw / volumes
-    n_full = counts_full / volumes
-
-    plt.loglog(r_centres[counts_mw > 0], n_mw[counts_mw > 0], label="MW only")
-    plt.loglog(r_centres[counts_full > 0], n_full[counts_full > 0], label="MW + LMC + NIF")
-    plt.xlabel("r (kpc)")
-    plt.ylabel("number density (kpc$^{-3}$)")
-    plt.title("Unperturbed vs perturbed radial density profile, N="+str(N))
-    plt.legend()
-    plt.savefig(filename)
-    plt.close()
-
-def plot_density_slices(dx,stars_mw,stars_full,ts_fwd,N,filename1="density_slice_mw_function.png",filename2="density_slice_full_function.png"):
-    # Plotting slice of the density in Y-Z at X=0
-
-    x_mw = stars_mw.x(ts_fwd[-1])
-    y_mw = stars_mw.y(ts_fwd[-1])
-    z_mw = stars_mw.z(ts_fwd[-1])
-
-    x_full = stars_full.x(ts_fwd[-1])
-    y_full = stars_full.y(ts_fwd[-1])
-    z_full = stars_full.z(ts_fwd[-1])
-
-    # Density slice in Y-Z, X=0
-    mask_mw_slice = np.abs(x_mw) < dx
-    mask_full_slice = np.abs(x_full) < dx
-
-    bins_yz = np.linspace(-500,500,40)
-
-    counts_mw, y_edges_mw, z_edges_mw = np.histogram2d(y_mw[mask_mw_slice],z_mw[mask_mw_slice],bins=bins_yz)
-    counts_full, y_edges_full, z_edges_full = np.histogram2d(y_full[mask_full_slice],z_full[mask_full_slice],bins=bins_yz)
-
-    log_counts_mw = np.where(counts_mw>0, np.log10(counts_mw),np.nan)
-    log_counts_full = np.where(counts_full>0, np.log10(counts_full),np.nan)
-
-    vmin = np.nanmin([log_counts_mw,log_counts_full])
-    vmax = np.nanmax([log_counts_mw,log_counts_full])
-
-    plt.pcolormesh(y_edges_mw, z_edges_mw, log_counts_mw.T, cmap="Blues",vmin=vmin,vmax=vmax)
-    plt.colorbar(label="star count (log)")
-    plt.xlabel("y (kpc)")
-    plt.ylabel("z (kpc)")
-    plt.title("MW only, y-z slice at |x| < "+str(dx)+" kpc, N="+str(N))
-
-    plt.savefig(filename1)
-    plt.close()
-
-    plt.pcolormesh(y_edges_full, z_edges_full, log_counts_full.T, cmap="Blues",vmin=vmin,vmax=vmax)
-    plt.colorbar(label="star count (log)")
-    plt.xlabel("y (kpc)")
-    plt.ylabel("z (kpc)")
-    plt.title("MW + LMC + NIF, y-z slice at |x| < "+str(dx)+" kpc, N="+str(N))
-    o_lmc_expected = Orbit.from_name("LMC")
-    plt.scatter(o_lmc_expected.y(), o_lmc_expected.z(),color='orange', s=100, marker='*', zorder=5, label="LMC")
-    plt.legend()
-
-    plt.savefig(filename2)
-    plt.close()
-
 def get_coords(stars):
     ts_fwd = np.linspace(0., 3., 2001) * u.Gyr
     x = stars.x(ts_fwd[-1])
@@ -669,6 +601,80 @@ def get_vr_vt(x,y,z,vx,vy,vz):
         vt_y = vy - vr*r_unit_y
         vt_z = vz - vr*r_unit_z
         return vr, vt_x, vt_y, vt_z
+
+def plot_radial_density_comparison(r_mw_final,r_full_final,N,filename="radial_density_comparsion_function.png"):
+
+    bins = np.geomspace(0.1,1000,50)
+    r_centres = 0.5 * (bins[:-1] + bins[1:])
+    volumes = (4.0/3.0) * np.pi * (bins[1:]**3 - bins[:-1]**3)
+
+    counts_mw, edges_mw = np.histogram(r_mw_final,bins=bins) 
+    counts_full, edges_full = np.histogram(r_full_final,bins=bins) 
+
+    n_mw = counts_mw / volumes
+    n_full = counts_full / volumes
+
+    plt.loglog(r_centres[counts_mw > 0], n_mw[counts_mw > 0], label="MW only")
+    plt.loglog(r_centres[counts_full > 0], n_full[counts_full > 0], label="MW + LMC + NIF")
+    plt.xlabel("r (kpc)")
+    plt.ylabel("number density (kpc$^{-3}$)")
+    plt.title("Unperturbed vs perturbed radial density profile, N="+str(N))
+    plt.legend()
+    plt.savefig(filename)
+    plt.close()
+
+def plot_density_slices(dx,stars_mw,stars_full,mw_interp,hq_df,N,filename1="density_slice_mw_function.png",filename2="density_slice_full_function.png"):
+    ts_bckwd = np.linspace(0, -3, 200) * u.Gyr
+    o_lmc_plot = Orbit.from_name("LMC")
+    friction = ChandrasekharDynamicalFrictionForce(GMs=1.5e11* u.Msun,sigmar=hq_df.sigmar)
+    o_lmc_plot.integrate(ts_bckwd, [mw_interp, friction], method="dop853_c")
+
+    lmc_y = o_lmc_plot.y(ts_bckwd)
+    lmc_z = o_lmc_plot.z(ts_bckwd)
+
+    # Plotting slice of the density in Y-Z at X=0
+
+    x_mw, y_mw, z_mw = get_coords(stars_mw)
+    x_full, y_full, z_full = get_coords(stars_full)
+
+    # Density slice in Y-Z, X=0
+    mask_mw_slice = np.abs(x_mw) < dx
+    mask_full_slice = np.abs(x_full) < dx
+
+    bins_yz = np.linspace(-500,500,30)
+
+    counts_mw, y_edges_mw, z_edges_mw = np.histogram2d(y_mw[mask_mw_slice],z_mw[mask_mw_slice],bins=bins_yz)
+    counts_full, y_edges_full, z_edges_full = np.histogram2d(y_full[mask_full_slice],z_full[mask_full_slice],bins=bins_yz)
+
+    counts_mw_nonzero = np.where(counts_mw>0, counts_mw, np.nan)
+    counts_full_nonzero = np.where(counts_full>0, counts_full, np.nan)
+
+    log_counts_mw = np.log10(counts_mw_nonzero)
+    log_counts_full = np.log10(counts_full_nonzero)
+
+    vmin = np.nanmin([log_counts_mw,log_counts_full])
+    vmax = np.nanmax([log_counts_mw,log_counts_full])
+
+    plt.pcolormesh(y_edges_mw, z_edges_mw, log_counts_mw.T, cmap="Blues",vmin=vmin,vmax=vmax)
+    plt.colorbar(label="star count (log)")
+    plt.xlabel("y (kpc)")
+    plt.ylabel("z (kpc)")
+    plt.title("MW only, y-z slice at |x| < "+str(dx)+" kpc, N="+str(N))
+
+    plt.savefig(filename1)
+    plt.close()
+
+    plt.pcolormesh(y_edges_full, z_edges_full, log_counts_full.T, cmap="Blues",vmin=vmin,vmax=vmax)
+    plt.colorbar(label="star count (log)")
+    plt.xlabel("y (kpc)")
+    plt.ylabel("z (kpc)")
+    plt.title("MW + LMC + NIF, y-z slice at |x| < "+str(dx)+" kpc, N="+str(N))
+    plt.scatter(o_lmc_plot.y(), o_lmc_plot.z(),color="orange", s=100, marker='*', zorder=5, label="LMC current position")
+    plt.plot(lmc_y,lmc_z,color="orange",linestyle="--",label="LMC orbit")
+    plt.legend()
+
+    plt.savefig(filename2)
+    plt.close()
 
 def plot_vdisp_profile_comparison(vr_mw,vr_full,r_mw_final,r_full_final,N,filename="vdisp_comparison_function.png"):
     bins = np.geomspace(0.1,1000,15)
@@ -761,14 +767,14 @@ def plot_orbital_anisotropy_comparison(v_disp_mw, vt_disp_mw,
     plt.savefig(filename)
     plt.close()
 
-def get_dispersion_slices(dx,x_mw,y_mw,z_mw,vr_mw,vt_mw,x_full,y_full,z_full,vr_full,vt_full):
+def get_dispersion_slices(dx,x_mw,y_mw,z_mw,vr_mw,vt_x_mw,vt_y_mw,vt_z_mw,x_full,y_full,z_full,vr_full,vt_x_full,vt_y_full,vt_z_full):
 
     mask_mw_slice = np.abs(x_mw) < dx
     mask_full_slice = np.abs(x_full) < dx
 
-    bins_yz = np.linspace(-500, 500, 40)
+    bins_yz = np.linspace(-500, 500, 30)
 
-    def dispersion_2d(y, z, v, bins):
+    def dispersion_2d_radial(y, z, v, bins):
         # get bin indices for each star
         y_idx = np.digitize(y, bins) - 1
         z_idx = np.digitize(z, bins) - 1
@@ -781,21 +787,46 @@ def get_dispersion_slices(dx,x_mw,y_mw,z_mw,vr_mw,vt_mw,x_full,y_full,z_full,vr_
                 if mask.sum() > 1:
                     sigma[i, j] = np.std(v[mask])
         return sigma
+    
+    def dispersion_2d_tangential(y, z, vx, vy, vz, bins):
+        y_idx = np.digitize(y, bins) - 1
+        z_idx = np.digitize(z, bins) - 1
+        n_bins = len(bins) - 1
+        sigma = np.full((n_bins, n_bins), np.nan)
 
-    sigma_r_mw_2d = dispersion_2d(y_mw[mask_mw_slice], z_mw[mask_mw_slice], vr_mw[mask_mw_slice], bins_yz)
-    sigma_r_full_2d = dispersion_2d(y_full[mask_full_slice], z_full[mask_full_slice], vr_full[mask_full_slice], bins_yz)
+        for i in range(n_bins):
+            for j in range(n_bins):
+                mask = (y_idx == j) & (z_idx == i)
+                if mask.sum() > 1:
+                    sigma_2 = np.var(vx[mask]) + np.var(vy[mask]) + np.var(vz[mask])
+                    sigma[i, j] = np.sqrt(sigma_2)
+        
+        return sigma
 
-    sigma_t_mw_2d = dispersion_2d(y_mw[mask_mw_slice], z_mw[mask_mw_slice], vt_mw[mask_mw_slice], bins_yz)
-    sigma_t_full_2d = dispersion_2d(y_full[mask_full_slice], z_full[mask_full_slice], vt_full[mask_full_slice], bins_yz)
+    sigma_r_mw_2d = dispersion_2d_radial(y_mw[mask_mw_slice], z_mw[mask_mw_slice], vr_mw[mask_mw_slice], bins_yz)
+    sigma_r_full_2d = dispersion_2d_radial(y_full[mask_full_slice], z_full[mask_full_slice], vr_full[mask_full_slice], bins_yz)
 
-    beta_mw_2d = 1 - (sigma_t_mw_2d**2 / (2*(sigma_r_mw_2d**2)))
-    beta_full_2d = 1 - (sigma_t_full_2d**2 / (2*(sigma_r_full_2d**2)))
+    sigma_t_mw_2d = dispersion_2d_tangential(y_mw[mask_mw_slice], z_mw[mask_mw_slice], vt_x_mw[mask_mw_slice], \
+                                             vt_y_mw[mask_mw_slice], vt_z_mw[mask_mw_slice], bins_yz)
+    sigma_t_full_2d = dispersion_2d_tangential(y_full[mask_full_slice], z_full[mask_full_slice], vt_x_full[mask_full_slice], \
+                                               vt_y_full[mask_full_slice], vt_z_full[mask_full_slice], bins_yz)
+
+    beta_mw_2d = 1 - (sigma_t_mw_2d**2) / (2*(sigma_r_mw_2d**2))
+    beta_full_2d = 1 - (sigma_t_full_2d**2) / (2*(sigma_r_full_2d**2))
 
     return sigma_r_mw_2d, sigma_r_full_2d, sigma_t_mw_2d, sigma_t_full_2d, beta_mw_2d, beta_full_2d
 
-def plot_vdisp_slices(dx,sigma_r_mw_2d,sigma_r_full_2d,N,filename1="vdisp_mw_slices_function.png",filename2="vdisp_full_slices_function.png"):
-    bins_yz = np.linspace(-500, 500, 40)
+def plot_vdisp_slices(dx,sigma_r_mw_2d,sigma_r_full_2d,hq_df,mw_interp,N,filename1="vdisp_mw_slices_function.png",filename2="vdisp_full_slices_function.png"):
+    bins_yz = np.linspace(-500, 500, 30)
     o_lmc_expected = Orbit.from_name("LMC")
+
+    ts_bckwd = np.linspace(0, -3, 200) * u.Gyr
+    o_lmc_plot = Orbit.from_name("LMC")
+    friction = ChandrasekharDynamicalFrictionForce(GMs=1.5e11* u.Msun,sigmar=hq_df.sigmar)
+    o_lmc_plot.integrate(ts_bckwd, [mw_interp, friction], method="dop853_c")
+
+    lmc_y = o_lmc_plot.y(ts_bckwd)
+    lmc_z = o_lmc_plot.z(ts_bckwd)
 
     log_sigma_r_mw_2d = np.where(sigma_r_mw_2d>0,np.log10(sigma_r_mw_2d),np.nan)
     log_sigma_r_full_2d = np.where(sigma_r_full_2d>0,np.log10(sigma_r_full_2d),np.nan)
@@ -804,7 +835,7 @@ def plot_vdisp_slices(dx,sigma_r_mw_2d,sigma_r_full_2d,N,filename1="vdisp_mw_sli
     vmax = np.nanmax([log_sigma_r_mw_2d,log_sigma_r_full_2d])
 
     plt.pcolormesh(bins_yz, bins_yz, log_sigma_r_mw_2d, cmap='Blues',vmin=vmin,vmax=vmax)
-    plt.colorbar(label="velocity dispersion (km/s)")
+    plt.colorbar(label="velocity dispersion (km/s) (log)")
     plt.xlabel("y (kpc)")
     plt.ylabel("z (kpc)")
     plt.title("MW, velocity dispersion at |x| < "+str(dx)+" kpc, N="+str(N))
@@ -812,17 +843,18 @@ def plot_vdisp_slices(dx,sigma_r_mw_2d,sigma_r_full_2d,N,filename1="vdisp_mw_sli
     plt.close()
 
     plt.pcolormesh(bins_yz, bins_yz, log_sigma_r_full_2d, cmap='Blues',vmin=vmin,vmax=vmax)
-    plt.colorbar(label="velocity dispersion (km/s)")
+    plt.colorbar(label="velocity dispersion (km/s) (log)")
     plt.xlabel("y (kpc)")
     plt.ylabel("z (kpc)")
     plt.title("MW+LMC+NIF, velocity dispersion at |x| < "+str(dx)+" kpc, N="+str(N))
-    plt.scatter(o_lmc_expected.y(), o_lmc_expected.z(), color='orange', s=100, marker='*', zorder=5, label="LMC")
+    plt.scatter(o_lmc_plot.y(), o_lmc_plot.z(),color="orange", s=100, marker='*', zorder=5, label="LMC current position")
+    plt.plot(lmc_y,lmc_z,color="orange",linestyle="--",label="LMC orbit")
     plt.legend()
     plt.savefig(filename2)
     plt.close()
 
 def plot_tdisp_slices(dx, sigma_t_mw_2d, sigma_t_full_2d,N,filename1="tdisp_mw_slices_function.png",filename2="tdisp_full_slices.png"):
-    bins_yz = np.linspace(-500, 500, 40)
+    bins_yz = np.linspace(-500, 500, 30)
     o_lmc_expected = Orbit.from_name("LMC")
 
     log_sigma_t_mw_2d = np.where(sigma_t_mw_2d>0,np.log10(sigma_t_mw_2d),np.nan)
@@ -850,7 +882,7 @@ def plot_tdisp_slices(dx, sigma_t_mw_2d, sigma_t_full_2d,N,filename1="tdisp_mw_s
     plt.close()
 
 def plot_oa_slices(dx, beta_mw_2d, beta_full_2d, N, filename1="oa_mw_slices_function.png", filename2="oa_full_slices_function.png"):
-    bins_yz = np.linspace(-500, 500, 40)
+    bins_yz = np.linspace(-500, 500, 30)
     o_lmc_expected = Orbit.from_name("LMC")
 
     plt.pcolormesh(bins_yz, bins_yz, beta_mw_2d, cmap='Blues',vmin=0,vmax=1)
@@ -871,7 +903,92 @@ def plot_oa_slices(dx, beta_mw_2d, beta_full_2d, N, filename1="oa_mw_slices_func
     plt.savefig(filename2)
     plt.close()
 
-def main(n=10000):
+def plot_density_ratio(dx,stars_mw,stars_full,N,filename="density_ratio_function.png"):
+    o_lmc_expected = Orbit.from_name("LMC")
+    bins_yz = np.linspace(-500, 500, 30)
+
+    x_mw, y_mw, z_mw = get_coords(stars_mw)
+    x_full, y_full, z_full = get_coords(stars_full)
+
+    mask_mw_slice = np.abs(x_mw) < dx
+    mask_full_slice = np.abs(x_full) < dx
+
+    counts_mw, y_edges, z_edges = np.histogram2d(y_mw[mask_mw_slice],z_mw[mask_mw_slice],bins=bins_yz)
+    counts_full, _, _ = np.histogram2d(y_full[mask_full_slice],z_full[mask_full_slice],bins=bins_yz)
+
+    counts_mw_nonzero = np.where(counts_mw>0, counts_mw, np.nan)
+    counts_full_nonzero = np.where(counts_full>0, counts_full, np.nan)
+
+    ratio = np.log10((counts_full_nonzero) / (counts_mw_nonzero))
+
+    max_deviation = np.nanmax(np.abs(ratio))
+    vmin = -max_deviation
+    vmax = max_deviation
+
+    """display_ratio = np.where((counts_mw > 0) & (counts_full > 0),
+                          counts_full / counts_mw,
+                    np.where((counts_mw == 0) & (counts_full > 0),
+                          vmax,  
+                    np.where((counts_mw > 0) & (counts_full == 0),
+                          vmin,  
+                          np.nan)))"""
+
+    plt.pcolormesh(y_edges, z_edges, ratio.T, cmap="RdBu_r",vmin=vmin,vmax=vmax)
+    plt.colorbar(label = "perturbed/unperturbed density ratio (log)")
+    plt.xlabel("y (kpc)")
+    plt.ylabel("z (kpc)")
+    plt.scatter(o_lmc_expected.y(), o_lmc_expected.z(), color="orange", s=100, marker="*", zorder=5, label="LMC")
+    plt.title("Perturbed to unperturbed density ratio at |x| < "+str(dx)+" kpc, N="+str(N))
+    plt.savefig(filename)
+    plt.close()
+
+def plot_radial_dispersion_ratio(dx,sigma_r_mw_2d, sigma_r_full_2d,N,filename="radial_dispersion_ratio_function.png"):
+    o_lmc_expected = Orbit.from_name("LMC")
+    bins_yz = np.linspace(-500,500,30)
+
+    sigma_mw_nonzero = np.where(sigma_r_mw_2d>0, sigma_r_mw_2d, np.nan)
+    sigma_full_nonzero = np.where(sigma_r_full_2d>0, sigma_r_full_2d, np.nan)
+
+    ratio = np.log10(sigma_full_nonzero / sigma_mw_nonzero)
+
+    max_deviation = np.nanmax(np.abs(ratio))
+    vmin = -max_deviation
+    vmax = max_deviation
+
+    plt.pcolormesh(bins_yz, bins_yz,ratio.T,cmap="RdBu_r",vmin=vmin,vmax=vmax)
+    plt.colorbar(label="perturbed/unperturbed $σ_r$ ratio (log)")
+    plt.xlabel("y (kpc)")
+    plt.ylabel("z (kpc)")
+    plt.scatter(o_lmc_expected.y(), o_lmc_expected.z(), color="orange", s=100, marker="*", zorder=5, label="LMC")
+    plt.title("Perturbed to unperturbed velocity dispersion ratio, |x| <"+str(dx)+" kpc, N="+str(N))
+    plt.legend()
+    plt.savefig(filename)
+    plt.close()
+
+def plot_tangential_dispersion_ratio(dx,sigma_t_mw_2d,sigma_t_full_2d,N,filename="tangential_dispersion_ratio_function.png"):
+    o_lmc_expected = Orbit.from_name("LMC")
+    bins_yz = np.linspace(-500,500,30)
+
+    sigma_mw_nonzero = np.where(sigma_t_mw_2d>0, sigma_t_mw_2d, np.nan)
+    sigma_full_nonzero = np.where(sigma_t_full_2d>0, sigma_t_full_2d, np.nan)
+
+    ratio = np.log10(sigma_full_nonzero / sigma_mw_nonzero)
+
+    max_deviation = np.nanmax(np.abs(ratio))
+    vmin = -max_deviation
+    vmax = max_deviation
+
+    plt.pcolormesh(bins_yz, bins_yz,ratio.T,cmap="RdBu_r",vmin=vmin,vmax=vmax)
+    plt.colorbar(label="perturbed/unperturbed $σ_t$ ratio (log)")
+    plt.xlabel("y (kpc)")
+    plt.ylabel("z (kpc)")
+    plt.scatter(o_lmc_expected.y(), o_lmc_expected.z(), color="orange", s=100, marker="*", zorder=5, label="LMC")
+    plt.title("Perturbed to unperturbed tangential dispersion ratio, |x| <"+str(dx)+" kpc, N="+str(N))
+    plt.legend()
+    plt.savefig(filename)
+    plt.close()
+
+def main(n=5000):
     gomp = ctypes.CDLL("libgomp.so.1")
     gomp.omp_set_num_threads(8)
 
@@ -923,21 +1040,22 @@ def main(n=10000):
     vr_full, vt_x_full, vt_y_full, vt_z_full = get_vr_vt(x_full,y_full,z_full,vx_full,vy_full,vz_full)
 
     plot_radial_density_comparison(r_mw_final,r_full_final,n)
-    plot_density_slices(20,stars_mw,stars_full,ts_fwd,n)
+    plot_density_slices(20,stars_mw,stars_full,mw_interp,hq_df,n)
     v_disp_mw, v_disp_full, counts_mw, counts_full = plot_vdisp_profile_comparison(vr_mw,vr_full,r_mw_final,r_full_final,n)
     vt_disp_mw, vt_disp_full, counts_t_mw, counts_t_full = plot_tangential_disp_comparison(vt_x_mw,vt_y_mw,vt_z_mw, \
         vt_x_full,vt_y_full,vt_z_full,r_mw_final,r_full_final,n)
     plot_orbital_anisotropy_comparison(v_disp_mw,vt_disp_mw,counts_mw,counts_t_mw,v_disp_full,vt_disp_full,counts_full,counts_t_full,n)
 
-    vt_mw = np.sqrt(vt_x_mw**2 + vt_y_mw**2 + vt_z_mw**2)
-    vt_full = np.sqrt(vt_x_full**2 + vt_y_full**2 + vt_z_full**2)
-
     sigma_r_mw_2d, sigma_r_full_2d, sigma_t_mw_2d, sigma_t_full_2d, beta_mw_2d, beta_full_2d = \
-          get_dispersion_slices(20,x_mw,y_mw,z_mw,vr_mw,vt_mw,x_full,y_full,z_full,vr_full,vt_full)
+          get_dispersion_slices(20,x_mw,y_mw,z_mw,vr_mw,vt_x_mw,vt_y_mw,vt_z_mw,x_full,y_full,z_full,vr_full,vt_x_full,vt_y_full,vt_z_full)
 
-    plot_vdisp_slices(20,sigma_r_mw_2d,sigma_r_full_2d,n)
+    plot_vdisp_slices(20,sigma_r_mw_2d,sigma_r_full_2d,hq_df,mw_interp,n)
     plot_tdisp_slices(20,sigma_t_mw_2d,sigma_t_full_2d,n)
     plot_oa_slices(20,beta_mw_2d,beta_full_2d,n)
+
+    plot_density_ratio(20,stars_mw,stars_full,n)
+    plot_radial_dispersion_ratio(20,sigma_r_mw_2d,sigma_r_full_2d,n)
+    plot_tangential_dispersion_ratio(20,sigma_t_mw_2d,sigma_t_full_2d,n)
 
 if __name__ == "__main__":
     main()
